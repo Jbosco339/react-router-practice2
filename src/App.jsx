3a6fa9f33730c02fbd20c59b60ead0a2fbc10d28
Jbosco339 <benjaminadakole20@gmail.com>
2026-02-03 07:14:51 +0100

    linked various pages via rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 
 function App() {
 
   return (
     <Router>
+      <nav>
+        <Link></Link>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </nav>
       <Routes>
-        <Route path='/'/>
-        <Route path='/about'/>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </Router>
     
