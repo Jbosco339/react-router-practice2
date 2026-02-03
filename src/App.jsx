402cccdda83379391c5a785a73d2f70386907752
Jbosco339 <benjaminadakole20@gmail.com>
2026-02-03 04:29:46 +0100

    reac-router-dom was imported and used

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
- 
 
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path='/'/>
+        <Route path='/about'/>
+      </Routes>
+    </Router>
+    
+  );
 }
 
 export default App
